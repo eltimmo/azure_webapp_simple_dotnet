9e4f1e794fe513968554328048b57dbddfec8f15
Tim Tharratt <tim.tharratt@coop.co.uk>
2023-11-10 09:05:33 +0000

    init

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.InteropServices;
+
+namespace dotnetcoresample.Pages;
+
+public class IndexModel : PageModel
+{
+
+    public string OSVersion { get { return RuntimeInformation.OSDescription; }  }
+    public string WebsiteName { get { return Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME"); }  }
+
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {        
+    }
+}
